57a4997f9d7cdd0fa970fc423a5ac23b5f922b7f
haifeng <haifeng.xue@here.com>
2018-09-06 09:47:57 +0200

    Fixed, external file is not injeted correctly if its path is an URL.

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function traverse(dir, list) {
 }
 
 function isURL(url){
-  return /^(((https|http|ftp|rtsp|mms):)?\/\/)+[A-Za-z0-9]+\.[A-Za-z0-9]+[/=?%\-&_~`@[\]':+!]*([^<>""])*$/.test(url);
+  return (new RegExp('^(?:[a-z]+:)?//', 'i')).test(url);
 }
 
 export default (opt = {}) => {
